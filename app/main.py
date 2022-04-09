00d55f71352e96931882bd44bf0816d08360186e
Hristo <aroquega@gmail.com>
2022-04-09 11:30:05 -0500

    arregla rutas relativas en los endpoints originales

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from typing import Optional, final
 from fastapi import Depends, FastAPI
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from typing import Optional, final
+from fastapi.responses import FileResponse
 from typing import List
 from sqlalchemy.orm import Session
 import requests
@@ -20,24 +20,23 @@ def get_db():
 
 app = FastAPI()
 
-'''
-app.mount("/resources", StaticFiles(directory="resources"), name="resources")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/resources", StaticFiles(directory="app/resources"), name="resources")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/")
 def read_root():
     get_wildfires()
-    return FileResponse("templates/index.html")
+    return FileResponse("app/templates/index.html")
 
 @app.get("/wildfires")
 def get_wildfires():
-    path="resources/wildfires.png"
+    path="app/resources/wildfires.png"
     if not os.path.exists(path):
         r = requests.get("https://maps.wild-fire.eu/gwis?LAYERS=viirs.hs&FORMAT=image/png&TRANSPARENT=true&SINGLETILE=false&SERVICE=wms&VERSION=1.1.1&REQUEST=GetMap&STYLES=&SRS=EPSG:4326&BBOX=-180.0,-90.0,180.0,90.0&WIDTH=1600&HEIGHT=1200&TIME=2021-12-11/2021-12-12")
         with open(path,'wb') as f:
             f.write(r.content)
     return {'path':path}
-'''
+
 
 @app.get("/disasters", response_model=List[schemas.Disaster])
 def get_disasters(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
